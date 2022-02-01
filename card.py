e1185ce2b2b77215f3b750d5cbfaf3c4b6f0e924
Sylwek_private <sojka.sylwester@gmail.com>
2022-02-01 19:04:38 +0100

    card

--- card.py
+++ card.py
@@ -1,2 +1,41 @@
 # card.py
 """Card class that represents a playing card and its image file name"""
+
+class Card:
+    FACES = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+    SUITS = ['Hearts', 'Clubs', 'Diamonds', 'Spades']
+
+    def __init__(self, face, suit):
+        self._face = face
+        self._suit = suit
+
+
+    @property
+    def face(self):
+        """Return the Card's self._face"""
+        return self._face
+
+    @property
+    def suit(self):
+        """Return the Card's self._face"""
+        return self._suit
+
+    @property
+    def image_name(self):
+        """Return image name"""
+        return str(self).replace(' ', '_') + 'png'
+
+    def __repr__(self):
+        """Return string representation for repr()."""
+        return f"Card(face='{self.face}', suit='{self.suit}')"
+
+    def __str__(self):
+        """Return strin representatin for str()."""
+        return f'{self.face} of {self.suit}'
+
+    def __format__(self, format):
+        """Return formatted string representation."""
+        return f'{str(self):{format}}'
+
+
+
