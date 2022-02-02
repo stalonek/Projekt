0887ddfe10bc8b098960bea728e0deb29af4ee73
Sylwek_private <sojka.sylwester@gmail.com>
2022-02-02 16:37:42 +0100

    karty

--- card.py
+++ card.py
@@ -1,14 +1,14 @@
 # card.py
-"""Card class that represents a playing card and its image file name"""
 
+"""Card class that represents a playing card and its image file name"""
 class Card:
-    FACES = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+    FACES = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
     SUITS = ['Hearts', 'Clubs', 'Diamonds', 'Spades']
 
     def __init__(self, face, suit):
         self._face = face
         self._suit = suit
-
 
     @property
     def face(self):
@@ -23,19 +23,16 @@ class Card:
     @property
     def image_name(self):
         """Return image name"""
-        return str(self).replace(' ', '_') + 'png'
+        return str(self).replace(' ', '_') + '.png'
 
     def __repr__(self):
         """Return string representation for repr()."""
         return f"Card(face='{self.face}', suit='{self.suit}')"
 
     def __str__(self):
-        """Return strin representatin for str()."""
+        """Return string representation for str()."""
         return f'{self.face} of {self.suit}'
 
     def __format__(self, format):
         """Return formatted string representation."""
         return f'{str(self):{format}}'
-
-
-
--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-from Time_class import Time
+from deck import DeckOfCards
+import matplotlip
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from pathlib import Path
 
-wake_up = Time()
-print(wake_up)
+deck_of_cards = DeckOfCards()
+deck_of_cards.shuffle()
 
-wake_up.time = (1,2,30)
-print(wake_up.time)
+path = Path('.').joinpath('card_images')
+
+figure, axes_list = plt.subplots(nrows=2, ncols=5)
+print(figure)
+print(axes_list)
+
+image_name = deck_of_cards.deal_card().image_name
+print(image_name)
+
+
+for axes in axes_list.ravel():
+    axes.get_xaxis().set_visible(False)
+    axes.get_yaxis().set_visible(False)
+    image_name = deck_of_cards.deal_card().image_name
+    img = mpimg.imread(str(path.joinpath(image_name).resolve()))
+    axes.imshow(img)
+
+figure.tight_layout()
+
+plt.show()
